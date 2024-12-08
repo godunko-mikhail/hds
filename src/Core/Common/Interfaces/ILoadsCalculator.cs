37911f5ce01203d37f91dfee382829622a86978f
godunko-mikhail <mm.godunko@gmail.com>
2024-12-08 21:58:28 +0300

    a bit of refactoring

--- src/Core/Common/Interfaces/ILoadsCalculator.cs
+++ src/Core/Common/Interfaces/ILoadsCalculator.cs
@@ -6,10 +6,12 @@ namespace Core.Common.Interfaces;
 public interface ILoadsCalculator<in TObj>
     where TObj : ILoadable, IPhysicMechanicalCharacteristic, IGeometricCharacteristic
 {
-    Task<FemModel> GetFirstGroupOfLimitStates(TObj model);
-    Task<FemModel> GetSecondGroupOfLimitStates(TObj model);
     IEnumerable<SegmentDisplacementMaximum> GetSegmentDisplacementMaximums(TObj model, FemModel fem);
     ForceMaximum GetForceMaximum(TObj model, FemModel fem);
+    
+    SupportReaction[] GetSupportReactions(TObj model, FemModel fem);
 }
 public record SegmentDisplacementMaximum(Node Node, double AbsoluteValue, double RelativeValue);
 public record ForceMaximum(double MaxForce, double MaxTangentialStress, double LoadingCoefficient);
+
+public record struct SupportReaction(double Force, double Weight);
